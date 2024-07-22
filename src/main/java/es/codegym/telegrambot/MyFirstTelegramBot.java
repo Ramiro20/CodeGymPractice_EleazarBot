dec172129db809dc1dc505cfc3d87b3177c3abc3
@Ramiro20 <leomar20rambito@gmail.com>
2024-07-22 09:06:30 -0400

    tarea Dia1 Finished

---
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -0,0 +1,28 @@
+package es.codegym.telegrambot;
+
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+public class MyFirstTelegramBot extends MultiSessionTelegramBot {
+
+    public static final String NAME = "CodeGymPractice_EleazarBot_bot";
+    public static final String TOKEN = "7293780837:AAGcNJiFwJVmibfqR23G-68Smjb5NRkkjtI";
+
+    public MyFirstTelegramBot() {
+        super(NAME, TOKEN);
+    }
+
+    @Override
+    public void onUpdateEventReceived(Update update) {
+        // TODO: escribiremos la funcionalidad principal del bot aquí
+        sendTextMessageAsync("Hola amigo , solo recordarte que tu puedes llegar a " +
+                "donde te imagines!");
+    }
+
+    public static void main(String[] args) throws TelegramApiException {
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+        telegramBotsApi.registerBot(new MyFirstTelegramBot());
+    }
+}
