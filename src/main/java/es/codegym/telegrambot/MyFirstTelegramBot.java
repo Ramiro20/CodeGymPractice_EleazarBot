004771f87552c06d2b135d7df533ac8a86538327
@Ramiro20 <leomar20rambito@gmail.com>
2024-07-22 23:45:20 -0400

    Dia 2 Finished

--- src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
+++ src/main/java/es/codegym/telegrambot/MyFirstTelegramBot.java
@@ -1,9 +1,13 @@
 package es.codegym.telegrambot;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.User;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.Objects;
 
 public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
@@ -16,9 +20,25 @@ public class MyFirstTelegramBot extends MultiSessionTelegramBot {
 
     @Override
     public void onUpdateEventReceived(Update update) {
+        Message mensaje = update.getMessage();
+        User usuario = mensaje.getFrom();
         // TODO: escribiremos la funcionalidad principal del bot aquí
-        sendTextMessageAsync("Hola amigo , solo recordarte que tu puedes llegar a " +
-                "donde te imagines!");
+        // si me escriben hola contestar  un saludo
+        if (getMessageText().equals("/start")){
+            sendTextMessageAsync("Hola _futuro_ *programador*!");}
+        if (getMessageText().contains("hola")){
+            sendTextMessageAsync("Hola ,Cual es tu nombre?");}
+        if (getMessageText().contains("me llamo")){
+            if(Objects.isNull(usuario.getUserName()))
+            {
+                sendTextMessageAsync("Mucho gusto");
+                sendTextMessageAsync("Yo soy "+getBotUsername()+" pero me puedes decir el señor *Gato*");
+                sendTextMessageAsync("Bienvenido: "+usuario.getFirstName());
+            }else{
+                sendTextMessageAsync("Mucho gusto");
+                sendTextMessageAsync("Yo soy "+getBotUsername()+" pero me puedes decir el señor *Gato*");
+                sendTextMessageAsync("Bienvenido: "+usuario.getFirstName()+" Usuario: "+usuario.getUserName());
+            }}
     }
 
     public static void main(String[] args) throws TelegramApiException {
